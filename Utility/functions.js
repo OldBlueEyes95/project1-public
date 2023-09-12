d57c32f350a4b5b7ee414af8b00aea2d99d4e1c7
InkLore <113934241+OldBlueEyes95@users.noreply.github.com>
2023-09-12 16:08:06 -0500

    JS style swap implemented for test site

--- Utility/functions.js
+++ Utility/functions.js
@@ -5,7 +5,6 @@ function button1() {
     document.getElementById("output").innerHTML = "Press x " + num;
 }
 
-
 function light_mode() {
     document.getElementById("page_style").setAttribute("href", "light.css");
 }
@@ -18,31 +17,35 @@ function toggleStyleSheet() {
     // Task 1
     // Steps
     // 1 (a) Get style element by ID (hint: getElementById)
-
     // 1 (b) Check the current stylesheet file name. (hint: element.getAttribute)
+    var cur_style = document.getElementById("page_style").getAttribute("href");
 
     // 1 (c) Determine new stylesheet file name
+    if (cur_style === "light.css") {
+        cur_style = "dark.css";
+    }
+    else if (cur_style === "dark.css") {
+        cur_style = "light.css";
+    }
 
     // 1 (d) replace stylesheet with new stylesheet (hint: element.setAttribute)
-    document.getElementById("MyElement").classList.toggle('MyClass');
+    // document.getElementById("MyElement").classList.toggle('MyClass');
+    document.getElementById("page_style").setAttribute("href", cur_style);
 
     // TASK 2
     // 2 (d) For persistence when page is refreshed. save new stylesheet name to localStorage
     // hint: localStorage.setItem(name, value)
+    localStorage.setItem("prev_style", cur_style);
 }
 
 
 window.onload = function(){
     // TASK 2
-    // TODO: Make the last div color persist even when someone refreshes the page.
-
     // Steps
     // 2 (a) get stylesheet name from local storage hint: localStorage.getItem(name)
-
     // 2 (b) get html style element by ID
-
     // 2 (c) replace href attribute of html element.
-    
+    document.getElementById("page_style").setAttribute("href", localStorage.getItem("prev_style"));
 }
 
 
@@ -56,11 +59,3 @@ document.getElementById("MyElement").classList.remove('MyClass');
 document.getElementById("MyElement").classList.contains('MyClass') )
 document.getElementById("MyElement").classList.toggle('MyClass');
 */
-
-function light_mode() {
-    document.getElementById('page_style').setAttribute("href", "light.css");
-}
-
-function dark_mode() {
-    document.getElementById('page_style').setAttribute("href", "dark.css");
-}
